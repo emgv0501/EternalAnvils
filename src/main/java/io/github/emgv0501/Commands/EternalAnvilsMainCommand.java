e15cd2e57cffcf72eb6384a24466474f403c0942
BuildTools <unconfigured@null.spigotmc.org>
2021-10-17 20:06:00 -0300

    Working on it

---
+++ src/main/java/io/github/emgv0501/Commands/EternalAnvilsMainCommand.java
@@ -0,0 +1,34 @@
+package io.github.emgv0501.Commands;
+
+import io.github.emgv0501.EternalAnvilsPlugin;
+import io.github.emgv0501.Utils.Utils;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.io.Console;
+
+public class EternalAnvilsMainCommand implements CommandExecutor {
+
+    public EternalAnvilsMainCommand(EternalAnvilsPlugin plugin){
+        plugin.getCommand("eternalanvils").setExecutor(this);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
+        Player player = (Player) sender;
+
+        if (!(sender instanceof Player)){
+            sender.sendMessage(Utils.format("&f&l[&3&lEternal&8&lAnvils&f&l]&c The plugin command can only be executed in game."));
+            return false;
+        }
+
+        if (args.length <= 0) return false;
+
+        if (args[0].equalsIgnoreCase("set")) {
+            player.sendMessage(Utils.format("&f&l[&3&lEternal&8&lAnvils&f&l]&e Right click on an anvil to make it indestructible."));
+        }
+        return false;
+    }
+}
--- src/main/java/io/github/emgv0501/EternalAnvilsPlugin.java
+++ src/main/java/io/github/emgv0501/EternalAnvilsPlugin.java
@@ -1,5 +1,6 @@
 package io.github.emgv0501;
 
+import io.github.emgv0501.Commands.EternalAnvilsMainCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class EternalAnvilsPlugin extends  JavaPlugin{
@@ -7,6 +8,7 @@ public class EternalAnvilsPlugin extends  JavaPlugin{
 
     @Override
     public void onEnable() {
+        new EternalAnvilsMainCommand(this);
         System.out.println("Enabling EternalAnvils.");
     }
 
